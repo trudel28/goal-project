ac44822281178729e1e39859e750750a12839c23
Olivier Trudel <trudelolivier1@hotmail.com>
2022-06-02 09:32:14 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const { errorHandler } = require('./middlewares/errorMiddleware')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
 
+connectDB()
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
